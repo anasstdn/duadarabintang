2eaccb4cd7cadbdfa48aa962bc5dd5ce90f1ebf1
Rasoki <salas.rasoki@gmail.com>
2024-05-11 19:26:16 +0700

    Update web content v1

--- application/language/indonesia/message_lang.php
+++ application/language/indonesia/message_lang.php
@@ -11,6 +11,10 @@ $lang['menu_contact'] = "Hubungi Kami";
 $lang['menu_bahasa'] = "Bahasa";
 /** END */
 
+/** HOME US MENU */
+$lang['content_home'] = "Selamat Datang di <br> PT. Dua Dara Bintang";
+/** END HOME US MENU */
+
 /** ABOUT US MENU */
 $lang['title_about_us'] = "Tentang Kami";
 /** INTRODUCTION SECTION */
@@ -21,20 +25,12 @@ $lang['content_intro'] .= "<p>Kebijakan Pemerintah terkait pengadaan alat keseha
 /** END INTRODUCTION SECTION */
 /** HISTORY SECTION */
 $lang['title_history'] = "Sejarah Dua Dara Bintang";
-$lang['content_history'] ="<p><b>PT. DUA DARA BINTANG</b> didirikan pada tanggal 12 Mei 2022 Oleh Andri Aryanata, Rosmini dan RR. Mutiara Citra sebagai pemegang saham <b>PT. DUA DARA BINTANG</b>.</p>";
+$lang['content_history'] ="<p><b>PT. DUA DARA BINTANG</b> didirikan pada tanggal 12 Mei 2022 Oleh A. Aryanata, R. Ki Bintang Cahya Putra, dan RR. Mutiara Citra sebagai pemegang saham <b>PT. DUA DARA BINTANG</b>.</p>";
 $lang['content_history'] .= "<p>Disaat pertumbuhan Rumah Sakit semakin pesat dan permintaan akan peralatan medis dan perdagangan umum menjadi sangat tinggi. <b>PT. DUA DARA BINTANG</b> menyediakan produk-produk dengan kwalitas yang baik didukung oleh profesional yang berpegalaman di bidang marketing dan admin support perusahaan.</p>";
 $lang['content_history'] .= "<p><b>PT. DUA DARA BINTANG</b> beralamat kantor : Jl. Tebet Utara Dalam No. 10 RT. 010, RW 001, Tebet Jakarta Selatan 12820.</p>";
-$lang['content_history'] .= "<p><b>PT. DUA DARA BINTANG</b> adalah sebuah perusahaan Importir, Supplier dan
-Distributor alat-alat kesehatan khususnya untuk kebutuhan kamar operasi, dimana
-perusahaan ini menjalin kerjasama dengan produsen / pabrikan alat kesehatan
-international yang tersebar dibenua Amerika, Eropa dan Asia sendiri untuk
-memenuhi kebutuhan permintaan Alat di Indonesia dan tentu saja alat tersebut telah
-memenuhi standar kualitas secara global</p>";
-$lang['content_history'] .= "<p>Kerjasama juga kami lakukan dengan beberapa perusahaan Alat Kesehatan Lokal di
-setiap daerah untuk memudahkan pendistribusian penyebaran produk- produk ke
-seluruh provinsi di Indonesia</p>";
-$lang['content_history'] .= "<p>Dan menyesuaikan dengan kebijakan pemerintahan saat ini dan kami juga
-memberikan harga produk yang bersaing di era BPJS dan E-Catalog.</p>";
+$lang['content_history'] .= "<p><b>PT. DUA DARA BINTANG</b> adalah sebuah perusahaan Importir, Supplier dan Distributor alat-alat kesehatan khususnya untuk kebutuhan kamar operasi, dimana perusahaan ini menjalin kerjasama dengan produsen / pabrikan alat kesehatan international yang tersebar dibenua Amerika, Eropa dan Asia sendiri untuk memenuhi kebutuhan permintaan Alat di Indonesia dan tentu saja alat tersebut telah memenuhi standar kualitas secara global</p>";
+$lang['content_history'] .= "<p>Kerjasama juga kami lakukan dengan beberapa perusahaan Alat Kesehatan Lokal di setiap daerah untuk memudahkan pendistribusian penyebaran produk- produk ke seluruh provinsi di Indonesia</p>";
+$lang['content_history'] .= "<p>Dan menyesuaikan dengan kebijakan pemerintahan saat ini dan kami juga memberikan harga produk yang bersaing di era BPJS dan E-Catalog.</p>";
 /** END HISTORY SECTION */
 $lang['title_visi'] = "Visi";
 $lang['content_visi'] = "<p>Menjadi penyedia peralatan kesehatan yang terpercaya di Indonesia juga mempunyai daya saing dan kualitas yang unggul</p>";
@@ -45,6 +41,15 @@ $lang['content_misi'] = "<p><ol class='list-group-numbered'>
                             <li class='list-group-item'>Memberikan pelayanan terbaik kepada konsumen.</li>
                         </ol></p>";
 /** END ABOUT US MENU */
+
+/** PRODUCT MENU */
+$lang['title_product'] = "Produk";
+$lang['content_our_product'] = "Produk Kami";
+$lang['product_1'] = "Gunting Medis";
+$lang['product_1_description'] = "Digunakan untuk gunting jaringan, gunting bedah, gunting tali pusat, dll";
+$lang['product_2_description'] = "Digunakan untuk perbaikan dinding perut yang terkena hermia. Bahan nya semi rigid dan rigid";
+$lang['product_3_description'] = "Haemostatic steril yang dapat diserap sponge gelatine";
+/** END CONTACT US MENU */
 
 /** CONTACT US MENU */
 $lang['title_contact_us'] = "Hubungi Kami";
--- application/views/view/v1/home.php
+++ application/views/view/v1/home.php
@@ -3,45 +3,46 @@
       <div class="container">
           <div class="row gy-4 d-flex justify-content-between">
               <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                  <h2 data-aos="fade-up">Your Lightning Fast Delivery Partner</h2>
-                  <p data-aos="fade-up" data-aos-delay="100">Facere distinctio molestiae nisi fugit tenetur repellat non praesentium nesciunt optio quis sit odio nemo quisquam. eius quos reiciendis eum vel eum voluptatem eum maiores eaque id optio ullam occaecati odio est possimus vel reprehenderit</p>
+                  <h2 data-aos="fade-up"><?php echo $this->lang->line('content_home'); ?></h2>
+                    <img src="<?= base_url()?>assets/assets/img/logo_1.png" alt="" >
+                    <!-- <p data-aos="fade-up" data-aos-delay="100">Facere distinctio molestiae nisi fugit tenetur repellat non praesentium nesciunt optio quis sit odio nemo quisquam. eius quos reiciendis eum vel eum voluptatem eum maiores eaque id optio ullam occaecati odio est possimus vel reprehenderit</p> -->
 
-                  <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
+                  <!-- <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
                       <input type="text" class="form-control" placeholder="ZIP code or CitY">
                       <button type="submit" class="btn btn-primary">Search</button>
-                  </form>
+                  </form> -->
 
-                  <div class="row gy-4" data-aos="fade-up" data-aos-delay="400">
+                  <!-- <div class="row gy-4" data-aos="fade-up" data-aos-delay="400">
 
                       <div class="col-lg-3 col-6">
                           <div class="stats-item text-center w-100 h-100">
                               <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
                               <p>Clients</p>
                           </div>
-                      </div><!-- End Stats Item -->
+                      </div>End Stats Item
 
                       <div class="col-lg-3 col-6">
                           <div class="stats-item text-center w-100 h-100">
                               <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
                               <p>Projects</p>
                           </div>
-                      </div><!-- End Stats Item -->
+                      </div>End Stats Item
 
                       <div class="col-lg-3 col-6">
                           <div class="stats-item text-center w-100 h-100">
                               <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
                               <p>Support</p>
                           </div>
-                      </div><!-- End Stats Item -->
+                      </div>End Stats Item
 
                       <div class="col-lg-3 col-6">
                           <div class="stats-item text-center w-100 h-100">
                               <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
                               <p>Workers</p>
                           </div>
-                      </div><!-- End Stats Item -->
+                      </div>End Stats Item
 
-                  </div>
+                  </div> -->
               </div>
 
               <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
@@ -56,7 +57,7 @@
   <main id="main">
 
       <!-- ======= Featured Services Section ======= -->
-      <section id="featured-services" class="featured-services">
+      <!-- <section id="featured-services" class="featured-services">
           <div class="container">
 
               <div class="row gy-4">
@@ -69,7 +70,7 @@
                           <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
                       </div>
                   </div>
-                  <!-- End Service Item -->
+                  End Service Item
 
                   <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="100">
                       <div class="icon flex-shrink-0"><i class="fa-solid fa-truck"></i></div>
@@ -78,7 +79,7 @@
                           <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
                           <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
                       </div>
-                  </div><!-- End Service Item -->
+                  </div>End Service Item
 
                   <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="200">
                       <div class="icon flex-shrink-0"><i class="fa-solid fa-truck-ramp-box"></i></div>
@@ -87,15 +88,15 @@
                           <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
                           <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
                       </div>
-                  </div><!-- End Service Item -->
+                  </div>End Service Item
 
               </div>
 
           </div>
-      </section><!-- End Featured Services Section -->
+      </section>End Featured Services Section -->
 
       <!-- ======= About Us Section ======= -->
-      <section id="about" class="about pt-0">
+      <!-- <section id="about" class="about pt-0">
           <div class="container" data-aos="fade-up">
 
               <div class="row gy-4">
@@ -135,10 +136,10 @@
               </div>
 
           </div>
-      </section><!-- End About Us Section -->
+      </section>End About Us Section -->
 
       <!-- ======= Services Section ======= -->
-      <section id="service" class="services pt-0">
+      <!-- <section id="service" class="services pt-0">
           <div class="container" data-aos="fade-up">
 
               <div class="section-header">
@@ -157,7 +158,7 @@
                           <h3><a href="service-details.html" class="stretched-link">Storage</a></h3>
                           <p>Cumque eos in qui numquam. Aut aspernatur perferendis sed atque quia voluptas quisquam repellendus temporibus itaqueofficiis odit</p>
                       </div>
-                  </div><!-- End Card Item -->
+                  </div>End Card Item
 
                   <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                       <div class="card">
@@ -167,7 +168,7 @@
                           <h3><a href="service-details.html" class="stretched-link">Logistics</a></h3>
                           <p>Asperiores provident dolor accusamus pariatur dolore nam id audantium ut et iure incidunt molestiae dolor ipsam ducimus occaecati nisi</p>
                       </div>
-                  </div><!-- End Card Item -->
+                  </div>End Card Item
 
                   <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
                       <div class="card">
@@ -177,7 +178,7 @@
                           <h3><a href="service-details.html" class="stretched-link">Cargo</a></h3>
                           <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p>
                       </div>
-                  </div><!-- End Card Item -->
+                  </div>End Card Item
 
                   <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
                       <div class="card">
@@ -187,7 +188,7 @@
                           <h3><a href="service-details.html" class="stretched-link">Trucking</a></h3>
                           <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p>
                       </div>
-                  </div><!-- End Card Item -->
+                  </div>End Card Item
 
                   <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
                       <div class="card">
@@ -197,7 +198,7 @@
                           <h3><a href="service-details.html" class="stretched-link">Packaging</a></h3>
                           <p>Illo consequuntur quisquam delectus praesentium modi dignissimos facere vel cum onsequuntur maiores beatae consequatur magni voluptates</p>
                       </div>
-                  </div><!-- End Card Item -->
+                  </div>End Card Item
 
                   <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
                       <div class="card">
@@ -207,15 +208,15 @@
                           <h3><a href="service-details.html" class="stretched-link">Warehousing</a></h3>
                           <p>Quas assumenda non occaecati molestiae. In aut earum sed natus eatae in vero. Ab modi quisquam aut nostrum unde et qui est non quo nulla</p>
                       </div>
-                  </div><!-- End Card Item -->
+                  </div>End Card Item
 
               </div>
 
           </div>
-      </section><!-- End Services Section -->
+      </section>End Services Section -->
 
       <!-- ======= Call To Action Section ======= -->
-      <section id="call-to-action" class="call-to-action">
+      <!-- <section id="call-to-action" class="call-to-action">
           <div class="container" data-aos="zoom-out">
 
               <div class="row justify-content-center">
@@ -227,10 +228,10 @@
               </div>
 
           </div>
-      </section><!-- End Call To Action Section -->
+      </section>End Call To Action Section -->
 
       <!-- ======= Features Section ======= -->
-      <section id="features" class="features">
+      <!-- <section id="features" class="features">
           <div class="container">
 
               <div class="row gy-4 align-items-center features-item" data-aos="fade-up">
@@ -250,7 +251,7 @@
                           <li><i class="bi bi-check"></i> Ullam est qui quos consequatur eos accusamus.</li>
                       </ul>
                   </div>
-              </div><!-- Features Item -->
+              </div>Features Item
 
               <div class="row gy-4 align-items-center features-item" data-aos="fade-up">
                   <div class="col-md-5 order-1 order-md-2">
@@ -268,7 +269,7 @@
                           culpa qui officia deserunt mollit anim id est laborum
                       </p>
                   </div>
-              </div><!-- Features Item -->
+              </div>Features Item
 
               <div class="row gy-4 align-items-center features-item" data-aos="fade-up">
                   <div class="col-md-5">
@@ -283,7 +284,7 @@
                           <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
                       </ul>
                   </div>
-              </div><!-- Features Item -->
+              </div>Features Item
 
               <div class="row gy-4 align-items-center features-item" data-aos="fade-up">
                   <div class="col-md-5 order-1 order-md-2">
@@ -301,13 +302,13 @@
                           culpa qui officia deserunt mollit anim id est laborum
                       </p>
                   </div>
-              </div><!-- Features Item -->
+              </div>Features Item
 
           </div>
-      </section><!-- End Features Section -->
+      </section>End Features Section -->
 
       <!-- ======= Pricing Section ======= -->
-      <section id="pricing" class="pricing pt-0">
+      <!-- <section id="pricing" class="pricing pt-0">
           <div class="container" data-aos="fade-up">
 
               <div class="section-header">
@@ -331,7 +332,7 @@
                           </ul>
                           <a href="#" class="buy-btn">Buy Now</a>
                       </div>
-                  </div><!-- End Pricing Item -->
+                  </div>End Pricing Item
 
                   <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
                       <div class="pricing-item featured">
@@ -346,7 +347,7 @@
                           </ul>
                           <a href="#" class="buy-btn">Buy Now</a>
                       </div>
-                  </div><!-- End Pricing Item -->
+                  </div>End Pricing Item
 
                   <div class="col-lg-4" data-aos="fade-up" data-aos-delay="300">
                       <div class="pricing-item">
@@ -361,15 +362,15 @@
                           </ul>
                           <a href="#" class="buy-btn">Buy Now</a>
                       </div>
-                  </div><!-- End Pricing Item -->
+                  </div>End Pricing Item
 
               </div>
 
           </div>
-      </section><!-- End Pricing Section -->
+      </section>End Pricing Section -->
 
       <!-- ======= Testimonials Section ======= -->
-      <section id="testimonials" class="testimonials">
+      <!-- <section id="testimonials" class="testimonials">
           <div class="container">
 
               <div class="slides-1 swiper" data-aos="fade-up">
@@ -389,7 +390,7 @@
                                   <i class="bi bi-quote quote-icon-right"></i>
                               </p>
                           </div>
-                      </div><!-- End testimonial item -->
+                      </div>End testimonial item
 
                       <div class="swiper-slide">
                           <div class="testimonial-item">
@@ -405,7 +406,7 @@
                                   <i class="bi bi-quote quote-icon-right"></i>
                               </p>
                           </div>
-                      </div><!-- End testimonial item -->
+                      </div>End testimonial item
 
                       <div class="swiper-slide">
                           <div class="testimonial-item">
@@ -421,7 +422,7 @@
                                   <i class="bi bi-quote quote-icon-right"></i>
                               </p>
                           </div>
-                      </div><!-- End testimonial item -->
+                      </div>End testimonial item
 
                       <div class="swiper-slide">
                           <div class="testimonial-item">
@@ -437,7 +438,7 @@
                                   <i class="bi bi-quote quote-icon-right"></i>
                               </p>
                           </div>
-                      </div><!-- End testimonial item -->
+                      </div>End testimonial item
 
                       <div class="swiper-slide">
                           <div class="testimonial-item">
@@ -453,17 +454,17 @@
                                   <i class="bi bi-quote quote-icon-right"></i>
                               </p>
                           </div>
-                      </div><!-- End testimonial item -->
+                      </div>End testimonial item
 
                   </div>
                   <div class="swiper-pagination"></div>
               </div>
 
           </div>
-      </section><!-- End Testimonials Section -->
+      </section>End Testimonials Section -->
 
       <!-- ======= Frequently Asked Questions Section ======= -->
-      <section id="faq" class="faq">
+      <!-- <section id="faq" class="faq">
           <div class="container" data-aos="fade-up">
 
               <div class="section-header">
@@ -489,7 +490,8 @@
                                       Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
                                   </div>
                               </div>
-                          </div><!-- # Faq item-->
+                          </div>
+                          # Faq item
 
                           <div class="accordion-item">
                               <h3 class="accordion-header">
@@ -503,7 +505,8 @@
                                       Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
                                   </div>
                               </div>
-                          </div><!-- # Faq item-->
+                          </div>
+                          # Faq item
 
                           <div class="accordion-item">
                               <h3 class="accordion-header">
@@ -517,7 +520,8 @@
                                       Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
                                   </div>
                               </div>
-                          </div><!-- # Faq item-->
+                          </div
+                          ># Faq item
 
                           <div class="accordion-item">
                               <h3 class="accordion-header">
@@ -532,7 +536,8 @@
                                       Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
                                   </div>
                               </div>
-                          </div><!-- # Faq item-->
+                          </div>
+                          # Faq item
 
                           <div class="accordion-item">
                               <h3 class="accordion-header">
@@ -546,7 +551,8 @@
                                       Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
                                   </div>
                               </div>
-                          </div><!-- # Faq item-->
+                          </div
+                          ># Faq item
 
                       </div>
 
@@ -554,6 +560,6 @@
               </div>
 
           </div>
-      </section><!-- End Frequently Asked Questions Section -->
+      </section>End Frequently Asked Questions Section -->
 
   </main>
